0ef20af2057a3c5316a7fc8d7ad6feb6d3a0515b
unknown <xarodasimba@gmail.com>
2019-04-05 18:32:15 +0300

    allages sto js file oste na min dexetai kamia entoli ektos aptis 3 pou tou dinei to manual kai na kleinei to tab sto quit

--- toDO.js
+++ toDO.js
@@ -2,16 +2,19 @@ var toDo = [];
 window.setTimeout(function() {
   var input = prompt("What would You like to do?");
   while (input !== "quit") {
-    if (input === "list") {
+    if (input !== "new" && input !=="list" ) {
+      alert("pls enter a valid command!");
+    }
+    else if (input === "list") {
       console.log(toDo);
       alert(toDo);
     } else if (input === "new") {
       var action = prompt("Pls Add a new Item to the List");
       toDo.push(action);
-    } else {
-      var action = prompt("pls enter a valid command!");
     }
     var input = prompt("What would You like to do?");
   }
   alert("ok, You quit the App!");
+  //closes the current tab
+  window.top.close();
 },500)
