76241a636ece2ae1d2d1001800fb3130a685103b
unknown <xarodasimba@gmail.com>
2019-04-05 18:13:25 +0300

    created repo for toDO app

---
+++ toDO.js
@@ -0,0 +1,17 @@
+var toDo = [];
+window.setTimeout(function() {
+  var input = prompt("What would You like to do?");
+  while (input !== "quit") {
+    if (input === "list") {
+      console.log(toDo);
+      alert(toDo);
+    } else if (input === "new") {
+      var action = prompt("Pls Add a new Item to the List");
+      toDo.push(action);
+    } else {
+      var action = prompt("pls enter a valid command!");
+    }
+    var input = prompt("What would You like to do?");
+  }
+  alert("ok, You quit the App!");
+},500)
