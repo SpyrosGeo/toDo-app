46cdd0ba71b9946c9482b8399d7275518b8672f2
unknown <xarodasimba@gmail.com>
2019-04-08 18:02:50 +0300

    allages sto js file kai sto html,dimiourgia functions gia forEach

--- toDO.js
+++ toDO.js
@@ -2,19 +2,46 @@ var toDo = [];
 window.setTimeout(function() {
   var input = prompt("What would You like to do?");
   while (input !== "quit") {
-    if (input !== "new" && input !=="list" ) {
+    if (input !== "new" && input !== "list" && input !== "delete") {
       alert("pls enter a valid command!");
-    }
-    else if (input === "list") {
-      console.log(toDo);
-      alert(toDo);
+    } else if (input === "list") {
+      listTodo();
     } else if (input === "new") {
-      var action = prompt("Pls Add a new Item to the List");
-      toDo.push(action);
+      addTodo();
+    } else if (input === "delete") {
+      //ask for index
+      delTodo();
     }
     var input = prompt("What would You like to do?");
   }
   alert("ok, You quit the App!");
+
+
+
+  //Functions
+
+  function listTodo() {
+    toDo.forEach(function(todo, i) {
+      console.log(i + ": " + toDo[i]);
+      alert(toDo.join("\n"));
+    })
+  }
+  function addTodo() {
+    var action = prompt("Pls Add a new Item to the List");
+    toDo.push(action);
+    alert("Item Added!");
+  }
   //closes the current tab
-  window.top.close();
-},500)
+  // window.top.close();
+  function delTodo() {
+    var index = prompt("enter index of todo to delete!")
+    //delete the item
+    //splice()
+    toDo.splice(index, 1);
+    alert("Item Deleted");
+    toDo.forEach(function(todo, i) {
+      console.log(i + ": " + toDo);
+      alert("Current list is\n " + toDo.join("\n"));
+    })
+  }
+}, 500)
